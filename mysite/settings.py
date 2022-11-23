b6cc5ad7c5db37fba64ae5a242cf39a45b2ff79a
Your Name <you@example.com>
2022-11-23 19:02:57 +0300

    Added registrashion

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'users',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -52,9 +54,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'mysite.urls'
+
 STATICFILES_DIR = (
     os.path.join(BASE_DIR, "static"),
 )
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -127,3 +131,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
